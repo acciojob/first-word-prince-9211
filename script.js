b7b3cc86ca744c35ba6beafd100b4a75b1237694
prince kumar sacxena <86111082+prince-9211@users.noreply.github.com>
2025-07-12 18:00:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
   // your code here
+	s=s.trimStart();
+	let spacIndex=s.indexof(' ');
+	if(spacIndex ==== -1){
+		return s;
+	}
+	return s.substring(0,spacIndex)
+}
+function handleClick() {
+const input=document.getElementById("textInput").value;
+	const result=firstWord(input);
+	documnet.getElementById("output").textContent=result;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+ const s = prompt("Enter String:");
+ alert(firstWord(s));
